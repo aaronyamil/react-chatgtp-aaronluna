49a8194fae576af0dfa27d286142416d438051a6
Aaron Luna <aaron.yamil@gmail.com>
2023-05-18 19:56:07 -0400

    feat: layout and main ui

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ const App = () => {
       <section className="side-bar">
         <button> + New Chat</button>
         <ul className="history">
-          <li>
-            Aaron
-          </li>
+          <li>Aaron</li>
         </ul>
         <nav>
           <p>Made by Aaron Luna</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>LunaGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div>»</div>
+          </div>
+          <p className="info">
+            This is LunaGPT. Our goal is to practice ReactJS and use some API,
+            in this case the openAPI.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
